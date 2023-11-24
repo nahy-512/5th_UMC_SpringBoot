9bedb9a8520d458eaa846a4bae2331143ecc2af8
Nahyun Kim <hedghog1004@naver.com>
2023-11-24 09:13:56 +0900

    ✨ Feat: Week8 - 에러 핸들러 실습

---
+++ study/src/main/java/umc/study/service/TempQueryService.java
@@ -0,0 +1,6 @@
+package umc.study.service;
+
+public interface TempQueryService { // GET 요청에 대한 비즈니스 로직을 처리하는 곳
+
+    void CheckFlag(Integer flag);
+}
--- study/src/main/java/umc/study/web/controller/TempRestController.java
+++ study/src/main/java/umc/study/web/controller/TempRestController.java
@@ -1,21 +1,33 @@
 package umc.study.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.TempConverter;
+import umc.study.service.TempQueryService;
 import umc.study.web.dto.TempResponse;
 
 @RestController
 @RequestMapping("/temp")
 @RequiredArgsConstructor
 public class TempRestController {
+
+    private final TempQueryService tempQueryService;
+
     //TODO: 컨트롤러에서 @RestController와 @RequiredArgsConstructor가 둘이 어떤 시너지를 내는지, 어떤 역할을 하는지 이해하는 것이 매우 중요 (의존성 주입)
     @GetMapping("/test")
     public ApiResponse<TempResponse.TempTestDTO> testAPI() {
 
         return ApiResponse.onSuccess(TempConverter.toTempTestDTO());
+    }
+
+    @GetMapping("/exception")
+    public ApiResponse<TempResponse.TempExceptionDTO> exceptionAPI(@RequestParam Integer flag) {
+        tempQueryService.CheckFlag(flag);
+        return ApiResponse.onSuccess(TempConverter.toTempExceptionDTO(flag));
     }
 }

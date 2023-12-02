4bbcbe8acc8d3ff8bfc3f7eddb377f18a6d1d8d9
Nahyun Kim <hedghog1004@naver.com>
2023-12-02 11:42:39 +0900

    ✨ Feat: Week9 - 어노테이션을 활용한 validation 처리

---
+++ study/src/main/java/umc/study/validation/validator/CategoriesExistValidator.java
@@ -0,0 +1,37 @@
+package umc.study.validation.validator;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import umc.study.apiPayload.code.status.ErrorStatus;
+import umc.study.repository.FoodCategoryRepository;
+import umc.study.validation.annotation.ExistCategories;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CategoriesExistValidator implements ConstraintValidator<ExistCategories, List<Long>> {
+
+    private final FoodCategoryRepository foodCategoryRepository;
+
+    @Override
+    public void initialize(ExistCategories constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+
+    @Override
+    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
+        // 검증 대상인 List<Long>의 값을 가진 카테고리가 모두 데이터베이스에 있는지를 판단함. 없을 경우 false를 반환
+        boolean isValid = values.stream()
+                .allMatch(value -> foodCategoryRepository.existsById(value));
+
+        if (!isValid) {
+            context.disableDefaultConstraintViolation();
+            context.buildConstraintViolationWithTemplate(ErrorStatus.FOOD_CATEGORY_NOT_FOUND.toString()).addConstraintViolation();
+        }
+
+        return isValid;
+    }
+}
--- study/src/main/java/umc/study/web/dto/UserRequestDTO.java
+++ study/src/main/java/umc/study/web/dto/UserRequestDTO.java
@@ -1,20 +1,32 @@
 package umc.study.web.dto;
 
 import lombok.Getter;
+import umc.study.validation.annotation.ExistCategories;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public class UserRequestDTO {
 
     @Getter
     public static class JoinDto {
+        @NotBlank
         String name;    // 이름
+        @NotNull
         Integer gender; // 성별
+        @NotNull
         Integer birthYear;  // YYYY
+        @NotNull
         Integer birthMonth; // MM
+        @NotNull
         Integer birthDay;   // DD
+        @Size(min = 5, max = 12)
         String address; // 주소
+        @Size(min = 5, max = 12)
         String specAddress; // 상세 주소 (동, 호수)
+        @ExistCategories
         List<Long> preferCategory; // 선호 음식 종류
     }
 }

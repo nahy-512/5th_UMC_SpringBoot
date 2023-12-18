2c96fa851e8ca1c9a980c146bd58d88b46b81c02
Nahyun Kim <hedghog1004@naver.com>
2023-12-18 16:49:28 +0900

    ✨ Feat: Week10 - 리뷰 목록 조회 서비스 매서드 로직 작성 및 리포지토리 메서드 작성

--- study/src/main/java/umc/study/validation/validator/StoreExistValidator.java
+++ study/src/main/java/umc/study/validation/validator/StoreExistValidator.java
@@ -3,18 +3,17 @@ package umc.study.validation.validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import umc.study.apiPayload.code.status.ErrorStatus;
-import umc.study.repository.StoreRepository;
+import umc.study.service.StoreService.StoreQueryService;
 import umc.study.validation.annotation.ExistStore;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
-import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class StoreExistValidator implements ConstraintValidator<ExistStore, List<Long>> {
+public class StoreExistValidator implements ConstraintValidator<ExistStore, Long> {
 
-    private final StoreRepository storeRepository;
+    private final StoreQueryService storeQueryService;
 
     @Override
     public void initialize(ExistStore constraintAnnotation) {
@@ -22,12 +21,10 @@ public class StoreExistValidator implements ConstraintValidator<ExistStore, List
     }
 
     @Override
-    public boolean isValid(List<Long> values, ConstraintValidatorContext context) {
-        // 검증 대상인 List<Long>의 값을 가진 카테고리가 모두 데이터베이스에 있는지를 판단함. 없을 경우 false를 반환
-        boolean isValid = values.stream()
-                .allMatch(storeRepository::existsById);
+    public boolean isValid(Long storeId, ConstraintValidatorContext context) {
+        boolean isValid = storeQueryService.existStore(storeId);
 
-        if (!isValid) {
+        if (!isValid){
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(ErrorStatus.STORE_NOT_FOUND.toString()).addConstraintViolation();
         }
--- study/src/main/java/umc/study/web/controller/StoreRestController.java
+++ study/src/main/java/umc/study/web/controller/StoreRestController.java
@@ -7,11 +7,14 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 import umc.study.apiPayload.ApiResponse;
 import umc.study.converter.StoreConverter;
+import umc.study.domain.Review;
 import umc.study.domain.Store;
 import umc.study.service.StoreService.StoreCommandService;
+import umc.study.service.StoreService.StoreQueryService;
 import umc.study.validation.annotation.ExistStore;
 import umc.study.web.dto.review.ReviewResponseDTO;
 import umc.study.web.dto.store.StoreRequestDTO;
@@ -25,6 +28,8 @@ import javax.validation.Valid;
 public class StoreRestController {
 
     private final StoreCommandService storeCommandService;
+
+    private final StoreQueryService storeQueryService;
 
     // 지역에 가게 추가하기
     @PostMapping("/{regionId}")
@@ -42,13 +47,15 @@ public class StoreRestController {
     @ApiResponses({ // API 응답
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "COMMON200",description = "OK, 성공"),
             @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH003", description = "access 토큰을 주세요!",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "acess 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
-            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "acess 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH004", description = "access 토큰 만료",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "AUTH006", description = "access 토큰 모양이 이상함",content = @Content(schema = @Schema(implementation = ApiResponse.class))),
     })
     @Parameters({
-            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!")
+            @Parameter(name = "storeId", description = "가게의 아이디, path variable 입니다!"),
+            @Parameter(name = "page", description = "페이지 번호, 0번이 1 페이지 입니다."),
     })
-    public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getReviewList(@ExistStore @PathVariable(name = "storeId") Long storeId){
-        return null;
+    public ApiResponse<ReviewResponseDTO.ReviewPreViewListDTO> getReviewList(@ExistStore @PathVariable(name = "storeId") Long storeId, @RequestParam(name = "page") Integer page){
+        Page<Review> reviewList = storeQueryService.getReviewList(storeId, page);
+        return ApiResponse.onSuccess(StoreConverter.reviewPreViewListDTO(reviewList));
     }
 }

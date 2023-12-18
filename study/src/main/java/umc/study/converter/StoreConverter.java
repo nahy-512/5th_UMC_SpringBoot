e0ac09582015021b257f4e818873d7ec8c8534e6
Nahyun Kim <hedghog1004@naver.com>
2023-12-18 16:49:28 +0900

    ✨ Feat: Week10 - 리뷰 목록 조회 서비스 매서드 로직 작성 및 리포지토리 메서드 작성
    Related to: #7

--- study/src/main/java/umc/study/converter/StoreConverter.java
+++ study/src/main/java/umc/study/converter/StoreConverter.java
@@ -1,5 +1,6 @@
 package umc.study.converter;
 
+import org.springframework.data.domain.Page;
 import umc.study.domain.Review;
 import umc.study.domain.Store;
 import umc.study.web.dto.review.ReviewResponseDTO;
@@ -8,6 +9,7 @@ import umc.study.web.dto.store.StoreResponseDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StoreConverter {
 
@@ -30,9 +32,26 @@ public class StoreConverter {
 
     // 10주차 가게 리뷰 목록 조회
     public static ReviewResponseDTO.ReviewPreViewDTO reviewPreViewDTO(Review review){
-        return null;
+        return ReviewResponseDTO.ReviewPreViewDTO.builder()
+                .ownerNickname(review.getUser().getName())
+                .score(review.getRate())
+                .createdAt(review.getCreatedAt().toLocalDate())
+                .body(review.getContent())
+                .build();
     }
-    public static ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(List<Review> reviewList){
-        return null;
+
+    public static ReviewResponseDTO.ReviewPreViewListDTO reviewPreViewListDTO(Page<Review> reviewList){
+
+        List<ReviewResponseDTO.ReviewPreViewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(StoreConverter::reviewPreViewDTO).collect(Collectors.toList());
+
+        return ReviewResponseDTO.ReviewPreViewListDTO.builder()
+                .isLast(reviewList.isLast())
+                .isFirst(reviewList.isFirst())
+                .totalPage(reviewList.getTotalPages())
+                .totalElements(reviewList.getTotalElements())
+                .listSize(reviewPreViewDTOList.size())
+                .reviewList(reviewPreViewDTOList)
+                .build();
     }
 }
---
+++ study/src/main/java/umc/study/service/StoreService/StoreQueryService.java
@@ -0,0 +1,16 @@
+package umc.study.service.StoreService;
+
+import org.springframework.data.domain.Page;
+import umc.study.domain.Review;
+import umc.study.domain.Store;
+
+import java.util.Optional;
+
+public interface StoreQueryService {
+
+    Optional<Store> findStore(Long id);
+
+    Page<Review> getReviewList(Long StoreId, Integer page);
+
+    boolean existStore(Long storeId);
+}
